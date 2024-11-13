caca1947544e08033e64021d066fd31dc8ce2592
BulizhnikGames <ksmolin06@gmail.com>
2024-11-13 16:19:36 +0300

    not allowing mega and super groups subscribe

--- fetcher/internal/fetcher/api.go
+++ fetcher/internal/fetcher/api.go
@@ -31,6 +31,9 @@ func (f *Fetcher) GetChannelInfo(ctx context.Context, channelName string) (int64
 		return 0, 0, false, errors.New("not a channel: got 0 chats by resolving")
 	}
 	if channel, ok := res.Chats[0].(*tg.Channel); ok {
+		if channel.Gigagroup || channel.Megagroup {
+			return 0, 0, false, errors.New("not a channel: invalid chat type - super/mega group")
+		}
 		return channel.ID, channel.AccessHash, !channel.Noforwards, nil
 	} else {
 		return 0, 0, false, errors.New(fmt.Sprintf("not a channel: invalid chat type (%T)", res.Chats[0]))
